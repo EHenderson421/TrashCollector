1467e94335c5d1f170545d481bc7a44f13e7a925
ehenderson421 <ehenderson421@gmail.com>
2018-06-07 14:46:33 -0500

    build schedule model

--- NewTrashCollector/Models/Schedule.cs
+++ NewTrashCollector/Models/Schedule.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,11 @@ namespace NewTrashCollector.Models
 {
     public class Schedule
     {
+        [Key]
+        public int ScheduleId { get; set; }
+
+        [Display(Name = "Pick Up Frequency")]
+        public string PickUpFrequency { get; set; }
+
     }
 }
