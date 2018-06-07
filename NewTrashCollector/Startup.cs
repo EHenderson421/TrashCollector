2cd96b6e5a534653d7b3f9282785d4fc999f3c94
ehenderson421 <ehenderson421@gmail.com>
2018-06-07 13:41:22 -0500

    first push

---
+++ NewTrashCollector/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NewTrashCollector.Startup))]
+namespace NewTrashCollector
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
