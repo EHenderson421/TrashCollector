1b8621e009572f7c859437c25053e283c4c8b5d2
ehenderson421 <ehenderson421@gmail.com>
2018-06-07 14:41:33 -0500

    apply foreign keys to customer model

--- NewTrashCollector/Models/Customer.cs
+++ NewTrashCollector/Models/Customer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -34,6 +35,15 @@ namespace NewTrashCollector.Models
 
         [Display(Name = "Bill Current")]
         public bool HasPaid { get; set; }
+
+        [ForeignKey("Address")]
+        public int AddressId { get; set; }
+        public Address Address { get; set; }
+
+        [ForeignKey("Invoice")]
+        public int InvoiceId { get; set; }
+        public Invoice Invoice { get; set; }
+
 
 
 
